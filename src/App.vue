4958da3abeb2366474eb88689a63bce0b3c21a62
mm <you@example.com>
2019-11-19 11:48:45 +0800

    整合element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-
+<el-button type="success" round>成功按钮</el-button>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ Vue.config.productionTip = false
 // 导入抽取的路由
 import router from './router/router.js'
 
+// 导入element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+
 new Vue({
   render: h =>h(App),
   router,
