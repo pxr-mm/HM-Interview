74640b39e9f5d95bc4610336c09d0b2da63ce3ae
mm <you@example.com>
2019-11-19 11:24:45 +0800

    整合路由

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// 导入抽取的路由
+import router from './router/router.js'
+
 new Vue({
-  render: h => h(App),
+  render: h =>h(App),
+  router,
 }).$mount('#app')
