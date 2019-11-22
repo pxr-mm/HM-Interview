0df071d20f9b5bf887f6b1bea52f8827a87bcecc
mm <you@example.com>
2019-11-22 10:51:00 +0800

    es6  axios抽取api层

---
+++ src/api/api.js
@@ -0,0 +1,33 @@
+// 1. 导入axios
+import axios from 'axios';
+// 2. 统一设置基地址
+axios.defaults.baseURL =  'http://183.237.67.218:3002';
+// 2.2 统一设置 跨域携带cookie
+axios.defaults.withCredentials = true;
+
+// 3. 暴露出去 有名字的暴露, 名字一般和接口地址一样
+export function login(data){
+    return axios ({
+        url:"/login",
+        method: "post",
+        data
+    })
+}
+
+// 暴露出去的 注册方法
+export function register(data) {
+    return axios({
+        url:"/register",
+        method: "post",
+        data
+    })
+}
+
+// 暴露出去的 获取短信验证码的方法
+export function sendsms(data){
+    return axios({
+        url:"/sendsms",
+        method:"post",
+        data
+    })
+}
